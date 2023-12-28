c0278ddfc9f58ffb60ca7360665cbd817507c802
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2023-12-28 22:42:20 +0100

    :rocket: :tada: TypeScript Playground  :rocket: :tada:

--- functions.ts
+++ functions.ts
@@ -6,3 +6,6 @@ export default addNumbers;
 
 
 export const addString = (str1: string, str2: string = ""): string => `${str1} ${str2}`;
+
+// Union Types
+export const format = (title: string , param: string | number) : string => `${title} ${param}`
