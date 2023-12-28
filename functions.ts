86ee694d498d2ac631cea7c28f7931bdb9eb9d59
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2023-12-28 23:27:40 +0100

    :rocket: :tada: TypeScript Playground  :rocket: :tada:

--- functions.ts
+++ functions.ts
@@ -9,3 +9,10 @@ export const addString = (str1: string, str2: string = ""): string => `${str1} $
 
 // Union Types
 export const format = (title: string , param: string | number) : string => `${title} ${param}`
+
+export const printFormat = (title: string , param: string | number): void =>{
+    console.log(format(title, param));
+}
+
+// Promeise Function    
+export const fetchData = (url: string): Promise<string> => Promise.resolve(`Data from ${url}`)
