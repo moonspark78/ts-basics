2677eece37ab228f874726bff0ab4ba07da4d014
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2023-12-29 19:40:29 +0100

    :triangular_flag_on_post:  TypeScript Playground :triangular_flag_on_post:

--- funcs-and-funcs.ts
+++ funcs-and-funcs.ts
@@ -6,10 +6,20 @@ export function printToFile(text: string, callback: () => void): void{
 
 
 type MuatationFunction = (v: number) => number;
+type AdderFunction = (v: number) => number;
 
 
-export function arrayMutate(numbers: number[], mutate: MuatationFunction): number[] {
+export function arrayMutate(numbers: number[], mutate: MuatationFunction): number[] { 
     return numbers.map(mutate)
 }
 
 console.log(arrayMutate([1,2,3], (v) => v *10));
+
+// Retourne une autre fonction 
+export function createAdder(num: number): AdderFunction {
+    return (val: number) =>  num + val;
+}
+
+
+const addOne = createAdder(1);
+console.log(addOne(55)); 
