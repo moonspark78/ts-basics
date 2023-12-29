c20835a491ba10ba0d7b982414dd5578bf5059db
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2023-12-29 19:27:35 +0100

    :triangular_flag_on_post:  TypeScript Playground :triangular_flag_on_post:

--- funcs-and-funcs.ts
+++ funcs-and-funcs.ts
@@ -3,3 +3,13 @@ export function printToFile(text: string, callback: () => void): void{
     callback();
 }
 
+
+
+type MuatationFunction = (v: number) => number;
+
+
+export function arrayMutate(numbers: number[], mutate: MuatationFunction): number[] {
+    return numbers.map(mutate)
+}
+
+console.log(arrayMutate([1,2,3], (v) => v *10));
